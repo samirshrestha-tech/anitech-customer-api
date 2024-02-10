5b03c975b8003f09c77e117ca13b77abb07d090c
samirshrestha-tech <128810359+samirshrestha-tech@users.noreply.github.com>
2024-02-11 00:01:53 +1100

    update on server config

--- server.js
+++ server.js
@@ -1,16 +1,40 @@
 import express from "express";
 
+import cors from "cors";
+
+import morgan from "morgan";
+
 const app = express();
+
+// middlewares
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use(morgan());
+
+// local middlewares
 
 const PORT = 8000;
 
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
   res.json({
     status: "success",
-    message: "your server is live",
+    message: "Your server is live",
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`your server is running at http://localhost:${PORT}`);
+app.use((error, req, res) => {
+  const errorCode = error.errorCode || 500;
+  res.status(errorCode).json({
+    status: "error",
+    message: error.message,
+  });
+});
+
+app.listen(PORT, (error) => {
+  error
+    ? console.log("error")
+    : console.log(`your server is running at http://localhost:${PORT}`);
 });
