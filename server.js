d3e84f849fcf849592648f21291d4073b59a7767
samirshrestha-tech <128810359+samirshrestha-tech@users.noreply.github.com>
2024-02-15 23:58:35 +1100

    update on database config and router congig

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import cors from "cors";
 
 import morgan from "morgan";
 
+import userRouter from "./src/router/userRouter.js";
+
 const app = express();
 
 // middlewares
@@ -17,6 +19,10 @@ app.use(morgan());
 // local middlewares
 
 const PORT = 8000;
+
+// router for user info
+
+app.use("/api/v1/users", userRouter);
 
 app.get("/", (req, res) => {
   res.json({
---
+++ src/router/userRouter.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+let router = express.Router();
+
+router.get("/", (req, res) => {});
+
+export default router;
